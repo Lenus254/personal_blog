23d38ef8373333e5a239c4c499ab28133f1c4e56
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-02-14 05:52:10 +0300

    initialized app instance and db

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
-from flask_sqlalchemy import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from app.config import Config
+from flask_migrate import Migrate
+from app.config import config_options
+
+
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+
+
+app = Flask(__name__)
+mail = Mail()
+db = SQLAlchemy()
 

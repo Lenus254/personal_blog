feeb028fade469bbfac1eefaad6e0f531d220ee9
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-02-14 06:20:33 +0300

    created classes in models and implemented relationships

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager
 from app.config import Config
 from flask_migrate import Migrate
 from app.config import config_options
+from flask_mail import Mail
 
 
 login_manager = LoginManager()
@@ -14,4 +15,29 @@ login_manager.login_message_category = 'info'
 app = Flask(__name__)
 mail = Mail()
 db = SQLAlchemy()
+
+def create_app(config_name):
+    
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    
+    app.config['SECRET_KEY'] = 'lenus254'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://lenus:2580/flaskblog'
+    print(app.config['SQLALCHEMY_DATABASE_URI'])
+    app.config['DEBUG']=True
+    db.init_app(app)
+    # bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    
+    
+    from app.users.views import users 
+    from app.posts.views import posts
+    from app.main.views import main 
+    
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
 

7f016bd13944ecdaaf48bb8cc2f7b62e70e940b3
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-02-16 09:43:26 +0300

    create routes in main views

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template, request, Blueprint
 from app.models import Post
 from app.requests import get_quote
 from .import main
-# main = Blueprint('main', __name__)
+
 
 
 @main.route("/")
@@ -11,7 +11,6 @@ def index():
     
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.posted_date.desc()).paginate(page=page, per_page=7)
-    # categories = Post.query.filter_by(category=post.category).all()
     myposts = Post.query.order_by(Post.posted_date.desc())
     return render_template('index.html', posts=posts, myposts=myposts ,quotes=quotes)
 

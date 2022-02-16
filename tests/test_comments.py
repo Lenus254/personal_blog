11a68da99d189d8c2faa926c9ed5fb3f019492e0
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-02-16 10:58:32 +0300

    added a code to test comments

---
+++ tests/test_comments.py
@@ -0,0 +1,31 @@
+from app.models import Comment, User
+from app import db
+
+
+def setUp(self):
+        self.user_James = User(username = 'lenas',password = 'banana', email = 'lenas@ms.com')
+        self.new_comment = Comment(post_id=12345,comment='This post is the best thing since sliced bread', user = self.user_lenas )
+  
+        
+def tearDown(self):
+        Comment.query.delete()
+        User.query.delete()
+        
+        
+def test_check_instance_variables(self):
+        self.assertEquals(self.new_comment.post_id,12345)
+        self.assertEquals(self.new_comment.comment,'This is comment test')
+        self.assertEquals(self.new_comment.user,self.user_lenas)
+        
+        
+def test_save_comment(self):
+        self.new_comment.save_comment()
+        self.assertTrue(len(Comment.query.all())>0)
+        
+        
+def test_get_comment_by_id(self):
+
+        self.new_comment.save_comment()
+        got_comments = Comment.get_comments(12345)
+        self.assertTrue(len(got_comments) == 1)
+        
